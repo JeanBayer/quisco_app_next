31ba12e0f1fe30644ec4bb622c7656be5b382419
Jhan Bayer <jhanbayer@gmail.com>
2022-06-09 12:34:42 -0400

    feat: query the database with ServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
-export default function Home() {
+import { PrismaClient } from "@prisma/client";
+
+export default function Home({ categorias }) {
+  console.log({ categorias });
   return <h1>next js</h1>;
 }
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient();
+  const categorias = await prisma.categoria.findMany();
+
+  return {
+    props: {
+      categorias,
+    },
+  };
+};
