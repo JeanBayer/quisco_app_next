0ea6ea2f780450a04dff3f5974baee75936f3338
Jhan Bayer <jhanbayer@gmail.com>
2022-06-20 18:11:55 -0400

    feat: add context

---
+++ context/QuioscoProvider.jsx
@@ -0,0 +1,12 @@
+import { createContext } from "react";
+
+const QuioscoContext = createContext();
+
+const QuioscoProvider = ({ children }) => {
+  return (
+    <QuioscoContext.Provider value={{}}>{children}</QuioscoContext.Provider>
+  );
+};
+
+export { QuioscoProvider };
+export default QuioscoContext;
