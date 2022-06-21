ca243506bdb504e72e77a4c0ad8716a1527ef11e
Jhan Bayer <jhanbayer@gmail.com>
2022-06-21 17:59:11 -0400

    feat: add modal

--- layout/Layout.js
+++ layout/Layout.js
@@ -1,7 +1,23 @@
 import Head from "next/head";
 import Sidebar from "components/Sidebar";
+import Modal from "react-modal";
+import useQuiosco from "hooks/useQuiosco";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+Modal.setAppElement("#__next");
 
 export default function Layout({ pagina, children }) {
+  const { modal, handleSetModal } = useQuiosco();
   return (
     <>
       <Head>
@@ -16,6 +32,10 @@ export default function Layout({ pagina, children }) {
           <div className="p-10">{children}</div>
         </main>
       </div>
+      <Modal isOpen={modal} style={customStyles}>
+        <h1>modal abierto</h1>
+        <button onClick={handleSetModal}>X</button>
+      </Modal>
     </>
   );
 }
